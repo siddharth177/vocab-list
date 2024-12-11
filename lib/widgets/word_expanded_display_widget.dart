66550ca8d7cf757e62eca91890d658cb68e4be58
= <=>
2024-12-11 13:39:59 +0530

    ui change

--- lib/widgets/word_expanded_display_widget.dart
+++ lib/widgets/word_expanded_display_widget.dart
@@ -15,11 +15,10 @@ class DisplayVocabListElement extends StatefulWidget {
 }
 
 class _DisplayVocabListElementState extends State<DisplayVocabListElement> {
-
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16.0),
+      padding: const EdgeInsets.all(10),
       child: Card(
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(12.0),
@@ -28,65 +27,70 @@ class _DisplayVocabListElementState extends State<DisplayVocabListElement> {
         clipBehavior: Clip.antiAliasWithSaveLayer,
         child: Padding(
           padding: const EdgeInsets.all(12),
-          child: SingleChildScrollView(
-            physics: const BouncingScrollPhysics(),
-            child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text(widget.wordMeaning.word[0].toUpperCase() + widget.wordMeaning.word.substring(1).toLowerCase(),
-                style: GoogleFonts.lato(
-                  fontSize: 40,
-                  fontWeight: FontWeight.w700
-                )),
-              Text(widget.wordMeaning.definition,
-                  style: GoogleFonts.poppins(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w300,
-                    fontStyle: FontStyle.italic
-                  )),
-              const SizedBox(
-                height: 20,
-              ),
-              Text(widget.wordMeaning.root,
-                  style: GoogleFonts.poppins(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w200,
-                      fontStyle: FontStyle.normal
-                  )),
-              const SizedBox(
-                height: 10,
-              ),
-              Text(widget.wordMeaning.phonatic,
-                  style: GoogleFonts.poppins(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w200,
-                      fontStyle: FontStyle.normal
-                  )),
-              const SizedBox(
-                height: 20,
-              ),
-              Text('Usages', style: GoogleFonts.roboto(
-                fontSize: 20
-              )),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+          child: SizedBox(
+            width: double.infinity,
+            child: SingleChildScrollView(
+              physics: const BouncingScrollPhysics(),
+              child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: widget.wordMeaning.usages.map((meaning) => Text(meaning)).toList(),
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                      widget.wordMeaning.word[0].toUpperCase() +
+                          widget.wordMeaning.word.substring(1).toLowerCase(),
+                      style: GoogleFonts.lato(
+                          fontSize: 40, fontWeight: FontWeight.w700)),
+                  if (widget.wordMeaning.definition.isNotEmpty)
+                    Text(widget.wordMeaning.definition,
+                        style: GoogleFonts.poppins(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w300,
+                            fontStyle: FontStyle.italic)),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  if(widget.wordMeaning.root.isNotEmpty)
+                    Text(widget.wordMeaning.root,
+                        style: GoogleFonts.poppins(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w200,
+                            fontStyle: FontStyle.normal)),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                  if(widget.wordMeaning.phonatic.isNotEmpty)
+                    Text(widget.wordMeaning.phonatic,
+                        style: GoogleFonts.poppins(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w200,
+                            fontStyle: FontStyle.normal)),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                  if(widget.wordMeaning.usages.isNotEmpty)
+                    Text('Usages', style: GoogleFonts.roboto(fontSize: 20)),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: widget.wordMeaning.usages
+                          .map((meaning) => Text(meaning))
+                          .toList(),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                  if(widget.wordMeaning.examples.isNotEmpty)
+                    Text('Examples', style: GoogleFonts.roboto(fontSize: 20)),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: widget.wordMeaning.examples
+                          .map((meaning) => Text(meaning))
+                          .toList(),
+                    ),
+                ],
               ),
-              const SizedBox(
-                height: 20,
-              ),
-              Text('Examples', style: GoogleFonts.roboto(
-                  fontSize: 20
-              )),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: widget.wordMeaning.examples.map((meaning) => Text(meaning)).toList(),
-              ),
-            ],
-                      ),
+            ),
           ),
         ),
       ),
